9f43cea31e52bd560078790814dde79e76073d34
sojib-dash <162636760+sojib-dash@users.noreply.github.com>
2026-02-19 01:59:09 +0600

    Update 1514B.cpp

--- Competitive-Programming/Binary-Number/1514B.cpp
+++ Competitive-Programming/Binary-Number/1514B.cpp
@@ -1,1 +1,20 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define MAX 1000000007
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        ll n,k,total=1;
+        cin >> n >> k;
+        for(int i=1;i<=k;i++){
+            total*=n;
+            total%=MAX;
+        }
+        cout << total << '\n';
+    }
+}
