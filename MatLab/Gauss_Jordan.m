a3c2b348c225c029178f17959788f97769f2a487
sojib-dash <162636760+sojib-dash@users.noreply.github.com>
2026-04-01 13:45:03 +0600

    Create Gauss_Jordan.m

---
+++ MatLab/Gauss_Jordan.m
@@ -0,0 +1,36 @@
+clear;clc;
+A = [1 1 1; 
+    2 3 1;
+    1 2 3]
+B = [6;
+    10;
+    13]
+Aug = [A B];
+[n m] = size(Aug);
+for i=1:n
+    if Aug(i,i) == 0
+        for k=i+1:n
+            if Aug(k,i) ~= 0
+                temp = A(i,:);
+                A(i,:) = Aug(k,:);
+                Aug(k,:) = temp;
+                break;
+            end
+        end
+    end
+    if Aug(i,i)==0
+        error('Infinite Solution Exist');
+    end
+    Aug(i,:) = Aug(i,:) / Aug(i,i);
+    for j=1:n
+        if j~=i
+            factor=Aug(j,i);
+            Aug(j,:) = Aug(j,:) - factor*Aug(i,:);
+        end
+    end
+end
+disp('Reduced Row Echelon Form:');
+disp(Aug);
+disp('X:')
+X=Aug(:,end);
+disp(X);
