4fd446099a9dacb5fab2de89b7e9dabd1482574b
sojib-dash <162636760+sojib-dash@users.noreply.github.com>
2026-04-06 03:38:08 +0600

    Update modified_euler_method.m

--- MatLab/Differential-Equations(ODEs)/modified_euler_method.m
+++ MatLab/Differential-Equations(ODEs)/modified_euler_method.m
@@ -1,15 +1,16 @@
+!Modified Euler method is also called predictor - corrector method
 clear;clc;
 f = @(x,y) x+y;
 x=0;
 y=1;
-h=0.1;
-n=10;
+h=0.001;
+n=1000;
 
 for i=1:n
     y_=y+h*f(x,y);
     y=y+h/2*(f(x,y)+f(x+h,y_));
     x=x+h;
-    fprintf('x = %.2f, y = %.5f\n', x, y);
+    fprintf('x = %.2f, y = %.10f\n', x, y);
 end
 disp('y = ');
 disp(y);
