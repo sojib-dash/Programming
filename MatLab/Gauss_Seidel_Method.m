45b7925b1f09848dfe8f4fec03319f21215d9a0f
sojib-dash <162636760+sojib-dash@users.noreply.github.com>
2026-04-05 16:54:32 +0600

    Create Gauss_Seidel_Method.m

---
+++ MatLab/Gauss_Seidel_Method.m
@@ -0,0 +1,31 @@
+! When the matrix is diagonally dominent
+! Modified Jacobi
+clear;clc;
+A = [10 5 1; 
+    7 11 2;
+    6 3 9]
+B = [6;
+    10;
+    13]
+n = length(B);
+x_ancient = zeros(n,1);
+tol=1e-6;
+max_iter=100;
+for t=1:max_iter
+    x_new=x_ancient;
+    for i=1:n
+        sum=0;
+        for j=1:n
+            if j~=i
+                sum = sum + A(i,j)*x_new(j);
+            end
+        end
+        x_new(i)=(B(i)-sum)/A(i,i);
+    end
+    if norm(x_new-x_ancient,inf)<tol
+        break
+    end
+    x_ancient=x_new;
+end
+disp('Solution:');
+disp(x_ancient);
